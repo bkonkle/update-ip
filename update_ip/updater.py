0d358f70143e043b56c8a2f0ac7dcb63cf4b7082
goncalopp <goncalopp@gmail.com>
2011-12-01 15:53:52 +0000

    create IPCheckerCache to separate updater/ip_caching logic

--- update_ip/updater.py
+++ update_ip/updater.py
@@ -14,6 +14,53 @@ IP_GETTERS=[DynDns(), WhatIsMyIp()]
 class InvalidServiceError(Exception):
     pass
 
+class IPCheckerCache(object):
+    def __init__(self, cache_filename, getters):
+        assert all( [isinstance(x, ip_getters.base.BaseIpGetter) for x in getters])
+        self.getters= getters
+        self.filename= cache_filename
+        try:
+            self._readFile()
+        except:
+            #file doesn't exist?
+            self.clear()
+
+    def _readFile(self):
+        f= open(self.filename, 'r')
+        self.last_ip= f.read()
+        f.close()
+
+    def _writeFile(self):
+        f= open( self.filename, 'w')
+        f.write( self.last_ip)
+        f.close()
+
+    def _getNewIp(self):
+        for getter in self.getters:
+            try:
+                return getter.get_ip()
+            except GetIpFailed:
+                pass
+        raise GetIpFailed("None of the ip_getters returned a good ip")
+
+    def has_changed(self):
+        '''checks for new ip, stores it in cache, returns boolean'''
+        current_ip= self._getNewIp()
+        if current_ip==self.last_ip:
+            return False
+        else:
+            self.last_ip= current_ip
+            self._writeFile()
+            return True
+
+    def current(self):
+        '''returns currently cached ip'''
+        return self.last_ip or None
+
+    def clear(self):
+        '''clears cache'''
+        self.last_ip=''
+        self._writeFile()
 
 class IPUpdater(object):
     def __init__(self, service, ip_file=None, quiet=False):
@@ -21,7 +68,7 @@ class IPUpdater(object):
             raise InvalidServiceError('Please provide a valid service to use '
                                       'for updating the domains.')
         self.service = service
-        self.ip_file = ip_file
+        self.cache= IPCheckerCache(ip_file, IP_GETTERS)
         self.quiet = quiet
     
     def update(self, domains=None, clear=False):
@@ -31,41 +78,35 @@ class IPUpdater(object):
         update messages for use in logging.
         """
         if clear:
-            self.clear_stored_ip()
-        
-        prev_ip = self.read_stored_ip()
+            self.cache.clear()
         
         # If no previous IP is found, then automatic domain detection will
         # fail.  Domains must be given if no ip_file is provided.
-        if not prev_ip and not domains:
+        if self.cache.current is None and domains is None:
             raise ValueError('No previous IP was found, and no domain '
                              'names were provided. Automatic domain '
                              'detection only works with a valid previous '
                              'IP.')
         
-        # Get the public IP and compare it to the previous IP, if provided
-        pub_ip = self.get_public_ip()
-        if prev_ip and pub_ip == prev_ip:
+        prev_ip= self.cache.current()
+        if not self.cache.has_changed():    #checks for new ip
             self.status_update('Public IP has not changed.')
             return
         else:
+            curr_ip= self.cache.current()
             self.status_update('Public IP has changed, updating %s.' %
                                self.service.name)
-            
-            # Update the ip_file
-            f = open(self.ip_file, 'w')
-            f.write(pub_ip)
-            f.close()
+
             
             # If no specific domains were provided, use the service interface
             # to get all the domains whose current IP matches the previous
             # public IP.
-            if not domains:
+            if domains is None:
                 domains = self.service.find_domains(prev_ip)
             
             for domain in domains:
-                self.status_update('\tUpdating %s to %s.' % (domain, pub_ip))
-                self.service.update(domain, pub_ip)
+                self.status_update('\tUpdating %s to %s.' % (domain, curr_ip))
+                self.service.update(domain, curr_ip)
     
     def status_update(self, message):
         """
@@ -76,28 +117,3 @@ class IPUpdater(object):
         now = now.strftime('%m/%d/%Y - %I:%M %p')
         if not self.quiet:
             print '%s: %s' % (now, message)
-    
-    def get_public_ip(self):
-        """Grab the current public IP."""
-        for getter in IP_GETTERS:
-            try:
-                return getter.get_ip()
-            except GetIpFailed:
-                pass
-        raise GetIpFailed("None of the ip_getters returned a good ip")
-
-    
-    def read_stored_ip(self):
-        if self.ip_file and os.path.exists(self.ip_file):
-            # Try to read the previous IP address
-            f = open(self.ip_file)
-            prev_ip = f.read()
-            f.close()
-            
-            return prev_ip
-    
-    def clear_stored_ip(self):
-        if self.ip_file and os.path.exists(self.ip_file):
-            # Remove the saved ip file
-            self.status_update('Removing the currently stored IP address.')
-            os.remove(self.ip_file)

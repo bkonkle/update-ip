610c18fe37490e4b2e6738d6a7e4fe15e5e6eae4
goncalopp <goncalopp@gmail.com>
2011-12-06 20:10:10 +0000

    configuration wizard, set inexistent options to None; write/read list as comma separated

--- update_ip/configuration.py
+++ update_ip/configuration.py
@@ -3,6 +3,7 @@ import ConfigParser
 class Configuration(object):
     SECTION= "update_ip"
     OPTIONS= ('cache_file', 'service_name', 'domains', 'service_username', 'service_password')
+    OPTIONS_DESCRIPTIONS= ('File where to cache last ip', 'Name of the updater service', 'Domains (comma-separated)', 'username for updater service', 'password for updater service')
     REQUIRED_OPTIONS= OPTIONS[:2]
     def __init__(self, **kwargs):
         options= {}
@@ -16,7 +17,13 @@ class Configuration(object):
             #validate all required options are present
             if not k in options:
                 raise Exception("Missing mandatory option: "+k)
+        for k in set(Configuration.OPTIONS).difference(kwargs.keys()):
+            #set options that were not given to None
+            options[k]= None
         self.__dict__= options #expose options as instance attributes, i.e: configuration.domains
+        
+        if self.domains:
+            self.domains= [x.strip() for x in self.domains.split(",")]
 
 
     def write_to_file(self, filename):
@@ -24,7 +31,10 @@ class Configuration(object):
         config = ConfigParser.RawConfigParser()
         config.add_section( self.SECTION )
         for k,v in self.__dict__.items():
-            config.set(self.SECTION , k, v)
+            if type(v)==list:
+                v=",".join(v)
+            if not v is None:
+                config.set(self.SECTION , k, v)
         with open(filename, 'wb') as configfile:
             config.write(configfile)
 
@@ -35,3 +45,24 @@ class Configuration(object):
         config.read(filename)
         file_options= dict( config.items( Configuration.SECTION ))
         return Configuration(**file_options)
+
+def configurationWizard():
+    def read_string( field_name, allow_empty= False ):
+        while True:
+            print field_name, ("[Required]" if not allow_empty else "")+":"
+            x= raw_input()
+            if x or allow_empty:
+                return x or None
+    print "Generating a new configuration file"
+    filename= read_string("Configuration filename to write")
+    options={}
+    for k, desc in zip( Configuration.OPTIONS, Configuration.OPTIONS_DESCRIPTIONS):
+        required= k in Configuration.REQUIRED_OPTIONS
+        v= read_string( "{0} ({1})".format(desc,k), allow_empty= not required)
+        options[k]= v
+    
+    print "Generating and writing configuration to file: ", filename
+    cfg= Configuration( **options )
+    cfg.write_to_file(filename)
+    print '''Finished. Please remember to set restrictive permissions \
+if the file contains sensitive data (like a service password)''' 

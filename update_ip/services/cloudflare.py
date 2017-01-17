f7f0adf9e2bcfa727fb8e9301589b1046863f1c8
goncalopp <goncalopp@users.noreply.github.com>
2017-01-17 23:43:18 +0000

    private methods with single underscore

--- update_ip/services/cloudflare.py
+++ update_ip/services/cloudflare.py
@@ -38,8 +38,8 @@ class CloudflareService(BaseDNSService):
         :param domain: Domain (or subdomain) to update
         :param ip: IP address to update
         """
-        self.__parse_domain(domain)
-        self.__update_ip(ip)
+        self._parse_domain(domain)
+        self._update_ip(ip)
 
     def find_domains(self, ip):
         """Not implemented, since in cloudflare you can have multiple domains, subdomains, dns-records ... it would
@@ -48,7 +48,7 @@ class CloudflareService(BaseDNSService):
         """
         raise NotImplementedError
 
-    def __parse_domain(self, domain):
+    def _parse_domain(self, domain):
         """Parse the given domain and extract the TLD
         :param domain: Full domain
         """
@@ -56,13 +56,13 @@ class CloudflareService(BaseDNSService):
         self.dns_record = domain
 
     @staticmethod
-    def __parse_result(self, data):
+    def _parse_result(self, data):
         """Parse the JSON result and return a dict/list with data
         :param data: JSON data to parse
         """
         return json.loads(data)
 
-    def __update_ip(self, ip, second_try=False):
+    def _update_ip(self, ip, second_try=False):
         """Update selected DNS record with provided IP
         :param ip: IP address to update
         :param second_try: If Record_ID is empty try to get it, if everything fails in a second attempt, we throw an
@@ -73,11 +73,11 @@ class CloudflareService(BaseDNSService):
                 raise DNSServiceError(
                     'Record ID is empty, we tried to find it, but it was not possible'
                 )
-            self.__get_dns_record()
-            self.__update_ip(ip, True)
+            self._get_dns_record()
+            self._update_ip(ip, True)
             return
 
-        request = self.__create_request(self.__update_url,
+        request = self._create_request(self._update_url,
                                         json.dumps({'name': self.dns_record, 'type': self.record_type, 'content': ip})
                                         )
 
@@ -86,16 +86,16 @@ class CloudflareService(BaseDNSService):
         result = opener.open(request)
         # data = self.parse_result(result.read())
 
-    def __get_zones(self):
+    def _get_zones(self):
         """Get the zone info for selected domain
         """
-        data = self.__api_get_request(self.__zones_list_url)
+        data = self._api_get_request(self._zones_list_url)
         if len(data[u'result']) > 0:
             self.zone_id = data[u'result'][0][u'id']
         else:
             raise DNSServiceError('No info found for domain: ' + self.domain)
 
-    def __get_dns_record(self, second_try=False):
+    def _get_dns_record(self, second_try=False):
         """Get the DNS Record info for selected sub-domain
         :param second_try: If Zone_ID is empty try to get it, if everything fails in a second attempt, we throw an
                             exception.
@@ -105,11 +105,11 @@ class CloudflareService(BaseDNSService):
                 raise DNSServiceError(
                     'Zone ID is empty, we tried to find it, but it was not possible'
                 )
-            self.__get_zones()
-            self.__get_dns_record(True)
+            self._get_zones()
+            self._get_dns_record(True)
             return
 
-        data = self.__api_get_request(self.__dns_list_url)
+        data = self._api_get_request(self._dns_list_url)
 
         if len(data[u'result']) > 0:
             self.record_id = data[u'result'][0][u'id']
@@ -118,14 +118,14 @@ class CloudflareService(BaseDNSService):
                 'No info found for DNS Record: "' + self.dns_record + '" with DNS type: "' + self.record_type + '"'
             )
 
-    def __api_get_request(self, url):
+    def _api_get_request(self, url):
         """Makes a GET request to API and return parsed JSON
         :param url: URL address
         """
-        request = self.__create_request(url)
-        return self.__parse_result(urllib2.urlopen(request))
+        request = self._create_request(url)
+        return self._parse_result(urllib2.urlopen(request))
 
-    def __create_request(self, url, data=None):
+    def _create_request(self, url, data=None):
         """Creates the HTTP request and add headers
         :param url: API URL with the petition.
         :param data: Request Data
@@ -135,10 +135,10 @@ class CloudflareService(BaseDNSService):
         else:
             request = urllib2.Request(url, data)
 
-        request = self.__add_url_headers(request)
+        request = self._add_url_headers(request)
         return request
 
-    def __add_url_headers(self, request):
+    def _add_url_headers(self, request):
         """Add headers to petition with auth data and other needed headers"""
         request.add_header('Content-Type', 'application/json')
         request.add_header('X-Auth-Key', self.api_key)
@@ -146,18 +146,18 @@ class CloudflareService(BaseDNSService):
         return request
 
     @property
-    def __zones_list_url(self):
+    def _zones_list_url(self):
         """Generate the URL for get the Zone List info"""
         return '{0}zones?name={1}'.format(self.api_url, self.domain)
 
     @property
-    def __dns_list_url(self):
+    def _dns_list_url(self):
         """Generate the URL for get the DNS List info"""
         return '{0}zones/{1}/dns_records?name={2}&type={3}'.format(self.api_url, self.zone_id, self.dns_record,
                                                                    self.record_type)
 
     @property
-    def __update_url(self):
+    def _update_url(self):
         """Generate the URL for update the DNS Record"""
         return '{0}zones/{1}/dns_records/{2}'.format(self.api_url, self.zone_id, self.record_id)
 

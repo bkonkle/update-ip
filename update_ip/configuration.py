09d7039c1122df26af990a4473e5a500774bc176
Cristian Deluxe <djcristiandeluxe@gmail.com>
2017-01-17 05:33:30 +0100

    Reformatted file with Python 2.x standards

--- update_ip/configuration.py
+++ update_ip/configuration.py
@@ -2,85 +2,89 @@ import ConfigParser
 import inspect
 from update_ip.services import services_by_name
 
-class InvalidConfigFile( Exception ):
+
+class InvalidConfigFile(Exception):
     pass
 
-class Configuration(object):
-    SECTION= "update_ip"
-    OPTIONS= ('cache_file', 'domains', 'service_name')
-    OPTIONS_DESCRIPTIONS= ('File where to cache last ip', 'Domains (comma-separated)', 'Name of the updater service')
-    REQUIRED_OPTIONS= OPTIONS[:2]
-    def __init__(self, **kwargs):
-        options= {}
-        for k,v in kwargs.items():
-            #read given options
-            options[k]=v
-        for k in set(Configuration.OPTIONS).difference(kwargs.keys()):
-            #set options that were not given to None
-            options[k]= None
-        self.__dict__= options #expose options as instance attributes, i.e: configuration.domains
-        
-        if self.domains:
-            self.domains= [x.strip() for x in self.domains.split(",")]
 
+class Configuration(object):
+    SECTION = 'update_ip'
+    OPTIONS = ('cache_file', 'domains', 'service_name')
+    OPTIONS_DESCRIPTIONS = ('File where to cache last ip', 'Domains (comma-separated)', 'Name of the updater service')
+    REQUIRED_OPTIONS = OPTIONS[:2]
+
+    def __init__(self, **kwargs):
+        options = {}
+        for k, v in kwargs.items():
+            # read given options
+            options[k] = v
+        for k in set(Configuration.OPTIONS).difference(kwargs.keys()):
+            # set options that were not given to None
+            options[k] = None
+        self.__dict__ = options  # expose options as instance attributes, i.e: configuration.domains
+
+        if self.domains:
+            self.domains = [x.strip() for x in str(self.domains).split(',')]
 
     def write_to_file(self, filename):
-        '''writes this configuration to a config file'''
+        """writes this configuration to a config file"""
         config = ConfigParser.RawConfigParser()
-        config.add_section( self.SECTION )
-        for k,v in self.__dict__.items():
-            if type(v)==list:
-                v=",".join(v)
+        config.add_section(self.SECTION)
+        for k, v in self.__dict__.items():
+            if type(v) == list:
+                v = ','.join(v)
             if not v is None:
-                config.set(self.SECTION , k, v)
+                config.set(self.SECTION, k, v)
         with open(filename, 'wb') as configfile:
             config.write(configfile)
 
     @staticmethod
     def read_from_file(filename):
-        '''creates a Configuration from a config file'''
+        """creates a Configuration from a config file"""
         try:
             config = ConfigParser.RawConfigParser()
             config.read(filename)
-            file_options= dict( config.items( Configuration.SECTION ))
+            file_options = dict(config.items(Configuration.SECTION))
             return Configuration(**file_options)
         except ConfigParser.NoSectionError as e:
             raise InvalidConfigFile(
-                "Failed to read configuration from '{0}': {1}".format(
-                    filename, e))
+                'Failed to read configuration from "{0}": {1}'.format(filename, e))
+
 
 def configurationWizard():
-    def read_string( field_name, allow_empty= False ):
+
+    def read_string(field_name, allow_empty=False):
         while True:
-            print field_name, ("[Required]" if not allow_empty else "")+":"
-            x= raw_input()
+            print field_name, ('[Required]' if not allow_empty else '') + ':'
+            x = raw_input()
             if x or allow_empty:
                 return x or None
-    print "Generating a new configuration file"
-    print "Available services:"+"\n  "+"\n  ".join(services_by_name.keys())
-    filename= read_string("Configuration filename to write")
-    options={}
-    for k, desc in zip( Configuration.OPTIONS, Configuration.OPTIONS_DESCRIPTIONS):
-        required= k in Configuration.REQUIRED_OPTIONS
-        v= read_string( "{0} ({1})".format(desc,k), allow_empty= not required)
-        options[k]= v
-    
+
+    print 'Generating a new configuration file'
+    print 'Available services:' + '\n  ' + '\n  '.join(services_by_name.keys())
+    filename = read_string('Configuration filename to write')
+    options = {}
+    for k, desc in zip(Configuration.OPTIONS, Configuration.OPTIONS_DESCRIPTIONS):
+        required = k in Configuration.REQUIRED_OPTIONS
+        v = read_string('{0} ({1})'.format(desc, k), allow_empty=not required)
+        options[k] = v
+
     svc_name = options['service_name']
     try:
-        service= services_by_name[svc_name]
+        service = services_by_name[svc_name]
     except KeyError:
-        print "Sorry, '%s' is not a valid service name" % (svc_name)
+        print 'Sorry, "%s" is not a valid service name' % svc_name
         exit(3)
-    
-    print "Service parameters:"
-    args, varargs, keywords, defaults= inspect.getargspec(service.__init__)
+
+    print 'Service parameters:'
+    args, varargs, keywords, defaults = inspect.getargspec(service.__init__)
     for a in args:
-        if a!='self':
-            v= read_string( a, allow_empty= False)
-            options[a]= v
-        
-    print "Generating and writing configuration to file: ", filename
-    cfg= Configuration( **options )
+        if a != 'self':
+            v = read_string(a, allow_empty=False)
+            options[a] = v
+
+    print 'Generating and writing configuration to file: ', filename
+    cfg = Configuration(**options)
     cfg.write_to_file(filename)
-    print '''Finished. Please remember to set restrictive permissions \
-if the file contains sensitive data (like a service password)''' 
+    print 'Finished. Please remember to set restrictive permissions if the file contains sensitive data ' + \
+          '(like a service password)'
--- update_ip/services/nfsn.py
+++ update_ip/services/nfsn.py
@@ -1,64 +1,67 @@
 from update_ip.services.base import BaseDNSService, DNSServiceError
+
 try:
     from pynfsn import pynfsn
 except ImportError:
     raise Exception("This service requires the pynfsn package. You can find it on pypi or github")
 
 import logging
-log= logging.getLogger('update_ip.services.nfsn')
 
-ALLOW_INEXISTENT= True #If this is false, I'll refuse to proceed if the domain record doesn't already exist
+log = logging.getLogger('update_ip.services.nfsn')
+
+ALLOW_INEXISTENT = True  # If this is false, I'll refuse to proceed if the domain record doesn't already exist
+
 
 def split_domain(domain):
     '''splits a complete domain into a pair of (subdomain, domain)'''
-    s= domain.split(".")
-    subdomain, domain= ".".join(s[:-2]), ".".join(s[-2:])
+    s = domain.split(".")
+    subdomain, domain = ".".join(s[:-2]), ".".join(s[-2:])
     return (subdomain, domain)
-        
+
+
 class NearlyFreeSpeechService(BaseDNSService):
     name = 'NearlyfreeSpeech'
-    TTL= 300    #5 minutes of dns record ttl
-    
+    TTL = 300  # 5 minutes of dns record ttl
+
     def __init__(self, username, api_key, **kwargs):
         if not username or not api_key:
             raise DNSServiceError('Username and api_key are required for the '
-                                 'NearlyFreeSpeech service.')
-        self.nfsn= pynfsn.NFSN(username, api_key)
-
+                                  'NearlyFreeSpeech service.')
+        self.nfsn = pynfsn.NFSN(username, api_key)
 
     def update(self, whole_domain, ip):
-        subdomain, domain= split_domain(whole_domain)
-        dns= self.nfsn.dns( domain )
+        subdomain, domain = split_domain(whole_domain)
+        dns = self.nfsn.dns(domain)
         try:
-            current_records= dns.listRRs(name=subdomain, type="A")
-            current_records= filter(lambda x:x['name']==subdomain, current_records)
+            current_records = dns.listRRs(name=subdomain, type="A")
+            current_records = filter(lambda x: x['name'] == subdomain, current_records)
         except Exception as e:
-            raise DNSServiceError("failed to get current records: "+str(e))
-        if len(current_records)>1:
-            raise DNSServiceError("Found more than one existing record with the given name: "+subdomain)
-        inexistent= len(current_records)==0
+            raise DNSServiceError("failed to get current records: " + str(e))
+        if len(current_records) > 1:
+            raise DNSServiceError("Found more than one existing record with the given name: " + subdomain)
+        inexistent = len(current_records) == 0
         if inexistent and not ALLOW_INEXISTENT:
-            raise DNSServiceError("Found no existing record with the given name: "+subdomain)
+            raise DNSServiceError("Found no existing record with the given name: " + subdomain)
         if inexistent:
-            log.warning("domain "+whole_domain+" did not exist - creating it")
+            log.warning("domain " + whole_domain + " did not exist - creating it")
         else:
-            record= current_records[0]
-            if record.get("type")!="A":
+            record = current_records[0]
+            if record.get("type") != "A":
                 raise DNSServiceError('Not an "A" record')
-            if record.get("name")!=subdomain:
+            if record.get("name") != subdomain:
                 raise DNSServiceError("Got a diferent record than expected")
-            record_ip= record.get("data")
-            #if record_ip==self.current_ip:
+            record_ip = record.get("data")
+            # if record_ip==self.current_ip:
             #    #"Record already up to date."
             #    return
             try:
-                dns.removeRR(name= subdomain, type=record.get("type"), data= record.get("data"))
+                dns.removeRR(name=subdomain, type=record.get("type"), data=record.get("data"))
             except Exception as e:
-                raise DNSServiceError("failed to remove: "+str(e))
+                raise DNSServiceError("failed to remove: " + str(e))
         try:
             dns.addRR(name=subdomain, type='A', data=ip, ttl=self.TTL)
         except Exception as e:
-            raise DNSServiceError("failed to add: "+str(e))
+            raise DNSServiceError("failed to add: " + str(e))
 
 
 service = NearlyFreeSpeechService
